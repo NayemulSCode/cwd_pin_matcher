252f4c4bfb721f1f971e4a6a16b5c453de9de4ba
Nayemul <nayemulhasan2016@gmail.com>
2021-01-31 20:35:12 +0600

    get all keys

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function generateInput(){
     const input = document.getElementById('inputValue');
     input.value = getPin();
 }
+// all key get by user
+const btnContainer = document.getElementById('inputKeys');
+btnContainer.addEventListener('click',function(event){
+    console.log(event.target.innerText);
+})
